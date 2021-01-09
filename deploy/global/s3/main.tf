e7837717ec15b5d9b75859fd5ef751a14bdae1be
iJackWilson <jack.wilson8796@gmail.com>
2021-01-09 15:37:05 +0000

    Add S3 default encryption

--- deploy/global/s3/main.tf
+++ deploy/global/s3/main.tf
@@ -12,6 +12,14 @@ resource "aws_s3_bucket" "b" {
     }
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+ }
+
   website {
     index_document = "index.html"
     error_document = "error.html"
