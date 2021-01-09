223bb49bc94f2bddb5c13e0c7d53cb29d5b42744
iJackWilson <jack.wilson8796@gmail.com>
2021-01-09 15:33:15 +0000

    Add lifecycle rule to bucket

--- deploy/global/s3/main.tf
+++ deploy/global/s3/main.tf
@@ -1,6 +1,16 @@
 resource "aws_s3_bucket" "b" {
   bucket = "private-url-shortener.jackwilson.uk"
   acl    = "public-read"
+  
+  lifecycle_rule {
+    id		= "DisposeShortUrls"
+    enabled	= true
+    prefix 	= "u/"
+    
+    expiration {
+      days = 365
+    }
+  }
 
   website {
     index_document = "index.html"
